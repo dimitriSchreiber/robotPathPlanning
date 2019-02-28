499c2226ecfef6a26dc583849a240412db7f9178
InspireX96 <xumw1996@gmail.com>
2019-02-27 23:15:12 -0800

    revised robot position

--- kinematics_controls/STLRead/torso_heatmap.m
+++ kinematics_controls/STLRead/torso_heatmap.m
@@ -53,9 +53,12 @@ V_rm = V_r / 1e3;
 % V_rm(:,1) = V_rm(:,1)-1.03;
 % V_rm(:,2) = V_rm(:,2)+0.8;
 % V_rm(:,3) = V_rm(:,3)-1.5;
-V_rm(:,1) = V_rm(:,1)-1.03;
-V_rm(:,2) = V_rm(:,2)+0.93;
-V_rm(:,3) = V_rm(:,3)-1;
+% V_rm(:,1) = V_rm(:,1)-1.03;
+% V_rm(:,2) = V_rm(:,2)+0.93;
+% V_rm(:,3) = V_rm(:,3)-1;
+V_rm(:,1) = V_rm(:,1)-1.13;
+V_rm(:,2) = V_rm(:,2)+0.6; % change y pos
+V_rm(:,3) = V_rm(:,3)-1;% change z pos
 
 ptCloud = pointCloud(V_rm);
 
@@ -64,17 +67,39 @@ save('Torso_vertices.mat','V_rm')
 
 %% load num_bins and create colormap
 num_bin = csvread('../notebooks/logs/num_bin.csv');
-colormap = zeros(size(num_bin,1),3);
-colormap(:,1) = num_bin/max(num_bin);       % adjust color here
+colormap_set = zeros(size(num_bin,1),3);
+colormap_set(:,1) = num_bin/max(num_bin);       % adjust color here
 %colormap(:,2) = num_bin/max(num_bin);
 %colormap(:,3) = num_bin/max(num_bin);
 
 %% Plot
 figure()
-scatter3(V_rm(:,1),V_rm(:,2),V_rm(:,3),36, colormap,'filled','s')
+scatter3(V_rm(:,1),V_rm(:,2),V_rm(:,3),36, colormap_set,'filled','s')
 colorbar('Ticks',[0,0.25,0.5,0.75,1],...
          'TickLabels',{'Cold','Cool','Neutral','Warm','Hot'})
 axis equal
 
-%% Save ply file
-plywrite('TorsoHotmap.ply',fv.faces,V_rm, uint8(colormap*255))
+% Save ply file
+plywrite('TorsoHotmap.ply',fv.faces,V_rm, uint8(colormap_set*255))
+
+%% Color map test
+nn = uint8(num_bin);
+nn_temp = ind2rgb(nn,jet);
+colormap_new(:,1) = nn_temp(:,:,1);
+colormap_new(:,2) = nn_temp(:,:,2);
+colormap_new(:,3) = nn_temp(:,:,3);
+
+fig = figure()
+colormap(jet)
+scatter3(V_rm(:,1),V_rm(:,2),V_rm(:,3),36, colormap_new,'filled','s')
+colorbar('Ticks',[0,0.25,0.5,0.75,1],...
+         'TickLabels',{'Cold','Cool','Neutral','Warm','Hot'})
+% colorbar('Ticks',[0,1],...
+%          'TickLabels',{'low dexterity','high dexterity'})
+axis equal
+
+saveas(fig,'heatmap_with_colorbar_2.fig')
+saveas(fig,'heatmap_with_colorbar.png')
+
+% Save ply file
+plywrite('TorsoHotmap_colorbar.ply',fv.faces,V_rm, uint8(colormap_new*255))
